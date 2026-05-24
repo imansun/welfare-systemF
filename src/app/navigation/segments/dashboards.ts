31584390de3c57217d7c125d9eb9d15fe641101f
imansun <134000751+imansun@users.noreply.github.com>
2026-05-24 10:07:23 +0330

    Update dashboards.ts

--- src/app/navigation/segments/dashboards.ts
+++ src/app/navigation/segments/dashboards.ts
@@ -26,6 +26,13 @@ export const dashboards: NavigationTree = {
       transKey: "nav.dashboards.companies",
       icon: "dashboards.companies",
     },
-    
+    {
+      id: "dashboards.units",
+      path: path(ROOT_DASHBOARDS, "/units"),
+      type: "item",
+      title: "Units",
+      transKey: "nav.dashboards.units",
+      icon: "dashboards.units",
+    },
   ],
 };
